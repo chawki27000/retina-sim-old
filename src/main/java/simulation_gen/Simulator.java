bcf0a88a8c6ca795a419e5cfb494cec04a5ab084
zahoussem <zahoussem@gmail.com>
2018-05-27 06:50:38 +0200

    Some proposals

--- src/main/java/simulation_gen/Simulator.java
+++ src/main/java/simulation_gen/Simulator.java
@@ -25,7 +25,6 @@ public class Simulator {
     }
 
     public void simulate() {
-
         Flit flit = null;
         while (!eventList.isEmpty() && clock < simulationPeriod) {
 
@@ -35,41 +34,47 @@ public class Simulator {
             Router router = curr_ev.getRouter_src();
 
             switch (curr_ev.getEventType()) {
-
-                case MESSAGE_SEND:
-                    message = curr_ev.getMessageSize();
-                    dest = curr_ev.getRouter_dest();
-                    router.sendMessage(message, dest);
-                    clock++; // ## clock ##
-                    break;
-
-                case SEND_FLIT:
-                    if (router.pe.isEmpty()) {
-                        break;
-                    }
-
-                    flit = router.pe.pullFlit();
-
-                    router.sendFlit(flit);
-                    clock++; // ## clock ##
-                    break;
-
-                case FORWARD_FLIT:
-                    // get the direction
-                    Direction direction = curr_ev.getDirection();
-
-                    if (direction == Direction.EAST)
-                        flit = router.getInRight().getVclist().get(0).dequeueFlit();
-                    else if (direction == Direction.WEST)
-                        flit = router.getInLeft().getVclist().get(0).dequeueFlit();
-                    else if (direction == Direction.NORTH)
-                        flit = router.getInUp().getVclist().get(0).dequeueFlit();
-                    else if (direction == Direction.SOUTH)
-                        flit = router.getInDown().getVclist().get(0).dequeueFlit();
-
-                    router.sendFlit(flit);
-                    clock++; // ## clock ##
-                    break;
+	    case MESSAGE_SEND:
+		message = curr_ev.getMessageSize();
+		dest = curr_ev.getRouter_dest();
+		router.sendMessage(message, dest);
+		creer_evenement_process_vc();
+		//          clock++; // ## clock ##
+		break;
+		    
+	    case SEND_HEADER_FLIT:
+		if (router.pe.isEmpty()) {
+		    break;
+		}
+		
+		flit = router.pe.pullFlit();		
+		router.sendFlit(flit);
+		// clock++; // ## clock ##
+		break;
+	    case SEND_FLIT:
+		// clock+1;
+		eliginleToSend();
+	        EST_dernierFLIT();
+		break;
+	    case SEND_TAIL:
+		
+		break;
+	    case FORWARD_FLIT:
+		// get the direction
+		Direction direction = curr_ev.getDirection();
+		
+		if (direction == Direction.EAST)
+		    flit = router.getInRight().getVclist().get(0).dequeueFlit();
+		else if (direction == Direction.WEST)
+		    flit = router.getInLeft().getVclist().get(0).dequeueFlit();
+		else if (direction == Direction.NORTH)
+		    flit = router.getInUp().getVclist().get(0).dequeueFlit();
+		else if (direction == Direction.SOUTH)
+		    flit = router.getInDown().getVclist().get(0).dequeueFlit();
+		
+		router.sendFlit(flit);
+		//                    clock++; // ## clock ##
+		break;
 
                 case RECEIVE_FLIT:
                     break;
